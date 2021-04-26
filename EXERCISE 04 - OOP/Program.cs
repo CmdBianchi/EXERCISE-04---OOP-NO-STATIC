e821f9421d5c980e5c28af32ceed6c1496e029fb
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-26 11:24:53 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 04 - OOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCISE_04___OOP {
+    //-----> INICIO
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            Calculator calc = new Calculator();
+
+            Console.Write("DIGITE O VALOR DO RAIO: "); double raio = double.Parse(Console.ReadLine());
+            double circ = calc.Circle(raio);
+            double volume = calc.Volume(raio);
+            Console.WriteLine("CIRCUNFERENCIA: " + circ.ToString("F2"));
+            Console.WriteLine("VOLUME: " + volume.ToString("F2"));
+            Console.WriteLine("VALOR DE PI: " +calc.Pi);
+        }
+
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> FIM
+}
